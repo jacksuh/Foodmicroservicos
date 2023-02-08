5a0b1fd1e870cfe12c88fb759f37efa3af0412c4
jacksuh <jacksonsdss@gmail.com>
2023-02-08 15:32:50 -0300

    Circuit Breaker e fallback

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.controller;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,7 +55,13 @@ public class PagamentoController {
     }
 
     @PatchMapping("/{id}/confirmar")
+    @CircuitBreaker(name = "atualizaPedido", fallbackMethod = "PagamentoAutorizadoComIntegracaoPendente")
     public void confirmarPagamento(@PathVariable @NotNull Long id){
         service.confirmarPagamento(id);
+    }
+
+    public void pagamentoAutorizadoComIntegracaoPendente(Long id, Exception e) {
+        service.alteraStatus(id);
+
     }
 }
--- src/main/java/br/com/alurafood/pagamentos/model/Status.java
+++ src/main/java/br/com/alurafood/pagamentos/model/Status.java
@@ -4,5 +4,8 @@ public enum Status {
 
     CRIADO,
     CONFIRMADO,
+
+    CONFIRMADO_SEM_INTEGRACAO,
+
     CANCELADO
 }
